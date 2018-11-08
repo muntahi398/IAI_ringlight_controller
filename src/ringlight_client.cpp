26e35013d37dabfc61db405e7a50c16660e2a70c
Alexis Maldonado <amaldo@cs.uni-bremend.de>
2018-11-09 00:25:02 +0100

    remove double ints from the client, it was only using one.

--- src/ringlight_client.cpp
+++ src/ringlight_client.cpp
@@ -5,28 +5,24 @@
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "iai_ringlight_client");
-  if (argc != 3)
+  if (argc != 2)
   {
-    ROS_INFO("usage: iai_ringlight_in_client + char X Y 'a'");
-    ROS_INFO("usage: iai_ringlight_in_client  char X Y ");
-    return 1;
+    ROS_INFO("usage: iai_ringlight_in_client X ");
+    return -1;
   }
 
   ros::NodeHandle n;
   ros::ServiceClient client = n.serviceClient<iai_ringlight_controller::iai_ringlight_in>("iai_ringlight_controller");
   iai_ringlight_controller::iai_ringlight_in srv;
   srv.request.a = atoll(argv[1]);
-  //srv.request.b = atoll(argv[2]);
-//  srv.request.c = atoll(argv[3]);
   if (client.call(srv))
   {
-    //ROS_INFO("Sum: %ld", (long int)srv.response.sum);
     ROS_INFO("Finished processing client.");
   }
   else
   {
     ROS_ERROR("Failed to call service iai_ringlight_controller");
-    return 1;
+    return -1;
   }
 
   return 0;
