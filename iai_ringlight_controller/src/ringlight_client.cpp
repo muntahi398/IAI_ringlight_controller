3d92959aae483f7718672d8807342d54de839467
ARRINA user on robot <ai-arrina@cs.uni-bremen.de>
2020-09-17 14:36:04 +0200

    Format some code to make it look nicer

--- iai_ringlight_controller/src/ringlight_client.cpp
+++ iai_ringlight_controller/src/ringlight_client.cpp
@@ -4,26 +4,26 @@
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "iai_ringlight_client");
-  if (argc != 2)
-  {
-    ROS_INFO("usage: iai_ringlight_in_client X ");
-    return -1;
-  }
+    ros::init(argc, argv, "iai_ringlight_client");
+    if (argc != 2)
+    {
+        ROS_INFO("usage: iai_ringlight_in_client X ");
+        return -1;
+    }
 
-  ros::NodeHandle n;
-  ros::ServiceClient client = n.serviceClient<iai_ringlight_msgs::iai_ringlight_in>("iai_ringlight_controller");
-  iai_ringlight_msgs::iai_ringlight_in srv;
-  srv.request.a = atoll(argv[1]);
-  if (client.call(srv))
-  {
-    ROS_INFO("Finished processing client.");
-  }
-  else
-  {
-    ROS_ERROR("Failed to call service iai_ringlight_controller");
-    return -1;
-  }
+    ros::NodeHandle n;
+    ros::ServiceClient client = n.serviceClient<iai_ringlight_msgs::iai_ringlight_in>("iai_ringlight_controller");
+    iai_ringlight_msgs::iai_ringlight_in srv;
+    srv.request.a = atoll(argv[1]);
+    if (client.call(srv))
+    {
+        ROS_INFO("Finished processing client.");
+    }
+    else
+    {
+        ROS_ERROR("Failed to call service iai_ringlight_controller");
+        return -1;
+    }
 
-  return 0;
+    return 0;
 }
